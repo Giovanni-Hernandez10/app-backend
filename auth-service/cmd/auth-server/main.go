c7957631ec28ce1e69a271ff3e3a98907a007e71
Giovanni-Hernandez10 <giovannihernandez10209@gmail.com>
2025-05-31 13:15:16 -0700

    successfully connected to postgresql database

---
+++ auth-service/cmd/auth-server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	db "github.com/Giovanni-Hernandez10/app-backend/auth-service/internal/db"
+	"github.com/joho/godotenv"
+)
+
+// entry point for the auth server
+func main() {
+
+	// loading env variables to use in our auth server
+	godotenv.Load("../../../.env")
+
+	// setting up db connection
+	conn, err := db.Connect()
+	if err != nil {
+		log.Fatalf("There was an error connecting to the database: %v", err)
+	}
+
+	defer conn.Close(context.Background())
+}
